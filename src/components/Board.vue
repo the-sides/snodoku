edc3f20d9cb8f7929a61dfd6c2063e23b0036023
the-sides <sides798@gmail.com>
2020-09-20 16:04:20 -0400

    Base, scss, eslint start on snowpack

---
+++ src/components/Board.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h4 class="">Board</h4>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Ui.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hidden">
+    <input type="checkbox" name="auto-solve" id="" />
+    <label for="auto-solve">Auto Solve</label>
+    <button>Solve</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
