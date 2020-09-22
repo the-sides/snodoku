2f401e82544d043e7f612636a5c1662a05e79b79
the-sides <sides798@gmail.com>
2020-09-22 18:39:34 -0400

    Center UI and hide debug info

--- src/components/Ui.vue
+++ src/components/Ui.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="my-6 max-w-sm mx-auto flex justify-around">
-    <div class="flex-1">
-      <input type="checkbox" name="auto-solve" id="" />
-      <label class="text-blue-200" for="auto-solve">Auto Solve</label>
+  <div class="">
+    <div class="my-6 max-w-sm mx-auto flex justify-around">
+      <div class="flex-1 flex justify-center">
+        <input type="checkbox" name="auto-solve" id="" />
+        <label class="text-blue-200 leading-3 my-auto ml-4 text-xl" for="auto-solve">Auto Solve</label>
+      </div>
+      <div class="flex-1">
+        <button
+          @click="solveBoard"
+          class="block mx-auto border-blue-400 border-2 bg-blue-200 py-2 px-4 rounded"
+        >
+          Solve
+        </button>
+      </div>
     </div>
-    <div class="flex-1">
-      <button @click="solveBoard" class="block mx-auto border-blue-400 border-2 bg-blue-200 py-2 px-4 rounded">Solve</button>
-    </div>
-    {{matrix}}
   </div>
 </template>
 
@@ -18,7 +24,7 @@ export default {
   },
   methods: {
     solveBoard() {
-      this.$emit('solveBoard');
+      this.$emit("solveBoard");
     }
   }
 };
