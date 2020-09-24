4dc4f4fbae6b0209ecfc6e321812e397d7147fa6
the-sides <sides798@gmail.com>
2020-09-24 15:19:51 -0400

    Board checks, box and utilities

--- src/features/solver.js
+++ src/features/solver.js
@@ -1,7 +1,31 @@
+function arrayUpTo(n) {
+  const rv = [];
+  let i = 1;
+  while (i - 1 < n) {
+    rv.push(i++);
+  }
+  return rv;
+}
+function remove(arr, val) {
+  // Passive fail on blank spots. 
+  if (!val) {
+    return arr;
+  }
+  const index = arr.indexOf(val);
+  // console.log('remove', val, index)
+  if (index == -1) return false;
+  arr.splice(index, 1);
+  return arr;
+}
+
 export default (board) => {
 
+  // Need to be here for the sake of sharing the scope.
+  // Ideally, the board should be a Class with the board member, operating via get/setters
+
   function getSpot(x, y) {
-    return board[x * 9 + y];
+    const val = Number(board[y * 9 + x]);
+    return val > 0 ? val : false;
   }
   function setSpot(x, y, val) {
     if ((x > 8) || (y > 8)) {
@@ -10,12 +34,48 @@ export default (board) => {
     board[y * 9 + x] = val;
   }
 
-  setSpot(0, 0, 5);
-  setSpot(1, 1, 5);
-  setSpot(2, 2, 5);
-  setSpot(7, 0, 5);
-  setSpot(-1, -1, 5);
-  setSpot(9, 9, 5);
+  ////////////////////////////////////////
+  // Using a point, check it's limitations, the point's row, col and containing box. 
+  const checkBox = (x, y) => {
+    // Identify box with coordinates that specify a point.
+    const xBox = Math.floor(x / 3);
+    const yBox = Math.floor(y / 3);
+    let remaining = arrayUpTo(9);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // console.log('inspecting x:',i + xBox * 3, ' y:', j + yBox * 3)
+        remaining = remove(remaining, getSpot(i + xBox * 3, j + yBox * 3));
+        // console.log(remaining)
+        if (!remaining) {
+          // console.log(`${xBox}, ${yBox}: FAIL X`);
+          return false;
+        }
+      }
+    }
+    // console.log(`${xBox}, ${yBox}: PASS √`);
+    return true;
+
+  }
+
+  const checkRow = () => {
+
+  }
+  const checkCol = () => {
+
+  }
+
+  const checkBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (!checkBox(i, j)) {
+          console.log('Board invalid')
+          return false;
+        }
+      }
+    }
+  }
+
+  checkBoard();
 
   return board;
 };
