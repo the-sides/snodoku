1c3555be0142f821c8790681f463b5ccc032fa41
Jacob Sides <jsides@pyxl.com>
2020-11-26 23:56:46 -0500

    Try simpler vue src

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Snodoku - A Sudoku solver built with Vue and Snowpack.
+# New Project
 
-> ✨ Bootstrapped with Create Snowpack App (CSA) - Vue edition.
+> ✨ Bootstrapped with Create Snowpack App (CSA).
 
 ## Available Scripts
 
-### yarn start
+### npm start
 
 Runs the app in the development mode.
 Open http://localhost:8080 to view it in the browser.
@@ -12,7 +12,15 @@ Open http://localhost:8080 to view it in the browser.
 The page will reload if you make edits.
 You will also see any lint errors in the console.
 
-### yarn build
+### npm test
+
+**⚠️ NOTE:** Vue 3 testing support is still in progress. This template does not ship with a test runner.
+
+### npm run build
 
 Builds the app for production to the `build/` folder.
 It correctly bundles Vue in production mode and optimizes the build for the best performance.
+
+### Q: What about Eject?
+
+No eject needed! Snowpack guarantees zero lock-in, and CSA strives for the same.
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,60 @@
 <template>
   <div class="App">
-    <header class="p-4 shadow-2xl bg-blue-200">
-      <h1 class="text-center text-4xl leading-7 my-0">Snodoku</h1>
-      <p class="text-center mt-2">
-        A snowpack application with vue, tailwind, and scss.
-        <br />
-        Built for someone learning a Sudoku solving algorithm by someone
-        learning Vue and new build tools.
+    <header class="App-header">
+      <img src="/logo.svg" class="App-logo" alt="logo" />
+      <p>
+        Edit
+        <code>src/App.vue</code> and save to reload.
       </p>
+      <a
+        class="App-link"
+        href="https://vuejs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >{{ message }}</a>
     </header>
-    <main class=" h-screen bg-blue-900 py-16 px-2 md:p-24">
-      <Board :matrix="matrix"></Board>
-      <Ui :matrix="matrix" @solveBoard="solveBoard"></Ui>
-    </main>
-    <footer></footer>
   </div>
 </template>
 
 <script>
-import Board from "./components/Board.vue";
-import Ui from "./components/Ui.vue";
-import solver from "./features/solver.js";
-
 export default {
-  components: {
-    Board,
-    Ui
-  },
   data() {
     return {
-      matrix: ['', 1, 7,'','','','',8,'','',6,3,8,9,5,7,1,'','','','','','',4,'','',1,6,'',5,'','','','',3,'',2,3,'',6,'',1,5,'',9,'','','','',8,'',2,6,'','',5,'','','','','','',6,7,1,8,5,2,3,'','','',9,'','','','',8,1,'']
+      message: "Learn Vue"
     };
-  },
-  methods: {
-    solveBoard() {
-      this.matrix = solver(this.matrix);
-    }
   }
 };
 </script>
 
-<style lang="postcss">
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+<style>
+.App {
+  text-align: center;
+}
+.App-header {
+  background-color: #f9f6f6;
+  color: #32485f;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+}
+.App-link {
+  color: #00c185;
+}
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+  margin-bottom: 1rem;
+  animation: App-logo-spin infinite 1.6s ease-in-out alternate;
+}
+@keyframes App-logo-spin {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.06);
+  }
+}
 </style>
